ac8827a9e8bdcddad733a04e606df956e8f1cba6
HuySon <huyson201@gmail.com>
2021-03-24 10:47:12 +0700

    update recyclerViewAdapter

---
+++ app/src/main/java/tdc/edu/vn/quanlynhansu/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,31 @@
+package tdc.edu.vn.quanlynhansu.adapters;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import tdc.edu.vn.quanlynhansu.data_models.Person;
+
+public class RecyclerViewAdapter extends  RecyclerView.Adapter {
+    private Activity context;
+    private int layoutId;
+    private ArrayList<Person> persons;
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+}
