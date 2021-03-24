ba87f1bb7b1e5d3f990274db285d6b7be98db2cc
HuySon <huyson201@gmail.com>
2021-03-24 11:21:20 +0700

    update func create viewholder

--- app/src/main/java/tdc/edu/vn/quanlynhansu/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/tdc/edu/vn/quanlynhansu/adapters/RecyclerViewAdapter.java
@@ -2,7 +2,9 @@ package tdc.edu.vn.quanlynhansu.adapters;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -28,7 +30,9 @@ public class RecyclerViewAdapter extends  RecyclerView.Adapter<RecyclerViewAdapt
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        LayoutInflater inflater = context.getLayoutInflater();
+        CardView cardView = (CardView) inflater.inflate(i, viewGroup, false);
+        return new MyViewHolder(cardView);
     }
 
     @Override
@@ -39,6 +43,11 @@ public class RecyclerViewAdapter extends  RecyclerView.Adapter<RecyclerViewAdapt
     @Override
     public int getItemCount() {
         return persons.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return layoutId;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
